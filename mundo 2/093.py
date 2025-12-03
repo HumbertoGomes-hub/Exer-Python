3a0ea82da9472253ded419a846d58193fa4c0a10
h1 <humbertoetiago@gmail.com>
2025-12-03 01:59:33 -0300

    Update 093.py

---
+++ mundo 2/093.py
@@ -0,0 +1,25 @@
+stats = dict()
+golspartida = dict()
+
+stats["nome"] = str(input('Nome: ')).title().strip()
+stats["partidas"] = int(input('Numero de partidas: '))
+for c in range(stats['partidas']):
+    golspartida[f"partida {c+1}"] = int(input(f"Quantos gol fez na partida {c+1}: "))
+stats["total de gols"] = sum(golspartida.values())
+
+print()
+
+for k,v in stats.items():
+    print(k,":",v)
+
+stats["gols por partida"] = golspartida.copy()
+print()
+
+for k,v in stats["gols por partida"].items():
+    print(f"{k} = Gols: {v}")
+    
+
+
+
+
+
