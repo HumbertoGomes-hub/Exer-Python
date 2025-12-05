456016545c3ab44b23fe4e0e3c943bd4ccbf2bed
h1 <humbertoetiago@gmail.com>
2025-12-05 18:37:11 -0300

    Create 095.py

---
+++ mundo 2/095.py
@@ -0,0 +1,39 @@
+stats = dict()
+golspartida = list()
+lista1 = list()
+gols = list()
+
+while True:
+    golspartida.clear()
+    stats["nome"] = str(input('Nome: ')).title().strip()
+    stats["partidas"] = int(input('Numero de partidas: '))
+    for c in range(stats['partidas']):
+        golspartida.append(int(input(f"Quantos gol fez na partida {c+1}: ")))
+    gols.append(golspartida.copy())
+    stats["total de gols"] = sum(golspartida.copy())
+    flag = str(input("QUER CONTINUAR [S/N]: ")).upper().strip()
+    lista1.append(stats.copy())
+    if flag[0] == "N":
+        break
+print()
+for k,v in enumerate(lista1):
+    print("ID  JOGADOR    GOLS    TOTAL")
+    print(f"{k}  {lista1[k]['nome']}        {gols[k]}        {lista1[k]['total de gols']}")
+print()
+print()
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
