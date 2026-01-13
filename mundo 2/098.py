0e8ba2dccde28203e0621e598987608a8aa3ffa7
h1 <humbertoetiago@gmail.com>
2026-01-13 16:35:28 -0300

    Update 098.py

--- mundo 2/098.py
+++ mundo 2/098.py
@@ -4,15 +4,15 @@ def contador(i , f, p): #i: inicio f: fim p: passo
         if p == 0:
             p = 1
         for c in range(i,f+1,p):
-            sleep(0.4)
+            #sleep(0.4)
             print(c, end=" ", flush=True)
         print("FIM!")
     elif f < i:
         if p == 0:
-            p = -1
+            p = 1
         p = -p
         for c in range(i,f-1,p):
-            sleep(0.4)
+            #sleep(0.4)
             print(c, end=" ", flush=True)
             
         print("FIM!")
