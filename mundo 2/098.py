b4989e654da6a6cf0c23d0a439e867e155e7ee0f
h1 <humbertoetiago@gmail.com>
2026-01-13 15:33:36 -0300

    Create 098.py

---
+++ mundo 2/098.py
@@ -0,0 +1,33 @@
+def contador(i , f, p): #i: inicio f: fim p: passo
+    from time import sleep
+    if f > i:
+        for c in range(i,f+1,p):
+            sleep(0.4)
+            print(c, end=" ")
+        print()
+    elif f < i:
+        for c in range(i,f-1,p):
+            sleep(0.4)
+            print(c, end=" ")
+            
+        print()
+    
+    
+def linha():
+    print("=" * 20)        
+
+        
+linha()   
+print("CONTAGEM 1 A 10 DE 1 EM 1")
+contador(1,10,1)
+linha()
+print("CONTAGEM 10 A 0 DE 2 EM 2")
+contador(10,0,-2)
+linha()
+inicio = int(input("INICIO: "))
+fim = int(input("FINAL: "))
+passo = int(input("PASSO: "))
+linha()
+print("CONTAGEM PERSONALIZADA")
+linha()
+contador(inicio, fim, passo)
