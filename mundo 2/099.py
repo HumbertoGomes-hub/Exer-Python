a3e5fc8ca7794e7d60d6ee82bfb0b73e2675e677
h1 <humbertoetiago@gmail.com>
2026-01-17 20:16:29 -0300

    update

--- mundo 2/099.py
+++ mundo 2/099.py
@@ -1,19 +1,15 @@
+from time import sleep
+
 def maior(*valores):
-    from time import sleep
-    a1 = "ANALISANDO VALORES"
-    print()
-    print("-=" * len(a1))
-    print(a1)
-    print("-=" * len(a1))
-    
+    print("-=" * 20)
+    print("ANALISANDO VALORES")
+    print("-=" * 20)
     if valores:
-        
         for c in valores:
             sleep(0.5)
             print(c, end=" ", flush=True)
         vmaior = max(valores)
         totv = len(valores)
-    
         print("SÃO OS VALORES", end="")
         print(f" TOTAL DE VALORES [{totv}] MAIOR VALOR [{vmaior}]")
     else:
@@ -25,5 +21,5 @@ def maior(*valores):
     
 maior(1,2,3,4,5,6)
 maior(17,62,98)
-maior(9,3,2,5)
+maior(1,1,0)
 maior()
--- mundo 2/100.py
+++ mundo 2/100.py
@@ -1,27 +1,26 @@
-def sorteia():
-    from random import randint
-    from time import sleep
-    
+from random import randint
+from time import sleep
+
+def sorteia(lista):
     for c in range(5):
-        numeros.append(randint(1,100))
+        lista.append(randint(1,10))
     print("SORTEANDO 5 VALORES ", end="")
-    for i in numeros:
+    for i in lista:
         sleep(0.4)
         print(i,flush=True, end=" ")
     print()
 
-def somaPar():
+def somaPar(lista):
     soma = list()
-    for c in numeros:
-        
+    for c in lista:
         if c%2 == 0:
             soma.append(c)
-    print(f"SOMANDO OS VALORES {numeros} A SOMA DOS PARES SÃO {sum(soma)}")
+    print(f"SOMANDO OS VALORES {lista} A SOMA DOS PARES SÃO {sum(soma)}")
             
             
           
             
 numeros = list()
-sorteia()
+sorteia(numeros)
 print("-=" * 30)
-somaPar()
+somaPar(numeros)
