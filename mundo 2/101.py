40c319a5a1dd39dac5f1600fbd2b2f6b1443bb9b
h1 <humbertoetiago@gmail.com>
2026-01-19 22:57:22 -0300

    Update 101.py

--- mundo 2/101.py
+++ mundo 2/101.py
@@ -1,20 +1,19 @@
-def voto():
+def voto(data2):
     from datetime import date
     ano_at = date.today().year
     global idade
-    idade = ano_at - data_nasc
+    idade = ano_at - data2
     if idade < 16:
-        return "NÃO VOTA"
-    elif idade >= 16 and idade < 65:
-        return "VOTO OBRIGATORIO"
+        return f"COM {idade} ANOS: NÃO VOTA."
+    elif idade >= 16 and idade < 18 or idade >= 65:
+        return f"COM {idade} ANOS: VOTO OPCIONAL."
     else:
-        return "VOTO OPCIONAL"
+        return f"COM {idade} ANOS: VOTO OBRIGATORIO."
     
     
     
 
 print("-" * 30)
 data_nasc = int(input("EM QUE ANO VOCE NASCEU? "))
-valor_voto = voto()
-print(f"COM {idade} ANOS: {valor_voto}.")
+print(voto(data_nasc))
 
