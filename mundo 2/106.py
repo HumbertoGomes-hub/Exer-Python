07f1dd73916cb3ee1edfa2020d04594eccb02160
h1 <humbertoetiago@gmail.com>
2026-01-20 13:50:58 -0300

    Update 106.py

--- mundo 2/106.py
+++ mundo 2/106.py
@@ -7,7 +7,7 @@ cores = {"verde": "\033[1;42m", "azul": "\033[1;44m",
 
 def ajuda(msg):
     from time import sleep
-    titulo(f"ACESSANDO MANUAL DO COMANDO {funcao}", cor=cores["azul"])
+    titulo(f" ACESSANDO MANUAL DO COMANDO {funcao} ", cor=cores["azul"])
     sleep(0.4)
     
     print(cores["amarelo"])
@@ -21,29 +21,17 @@ def titulo(msg2, cor=cores["remover"]):
     tam = len(msg2)+4
     print(cor)
     print("~" * tam)
-    print(msg2)
-    print("~" * tam,cores["remover"])
+    print(f"  {msg2}")
+    print("~" * tam, end="")
+    print(cores["remover"])
     
     
     
-
-
-
 # Programa Principal
-
-
 while True:
-    
-    
-    
-    
     titulo("SISTEMA DE AJUDA PyHelp", cor=cores["verde"])
-    
     funcao = str(input("Função ou Biblioteca > ")).lower().strip()
-    
     ajuda(funcao)
-    
-    
     if funcao == "fim":
         titulo("ATE LOGO!",cor=cores["vermelho"])
         break
