8e679ee4da5105c054921d8639277fa83a7f3f7f
h1 <humbertoetiago@gmail.com>
2026-01-20 00:12:06 -0300

    Update 102.py

--- mundo 2/102.py
+++ mundo 2/102.py
@@ -1,10 +1,42 @@
 def fatorial(num=0,show=False):
-    fat = 0
-    for c in range(num):
-        fat *= c
+    fat = num
+    if show == True:
+        
+        for c in range(num,0,-1):
+            if c < 5:
+                fat *= c
+
+            print(f"{c} ",end="")
+            if c == 1:
+                print(f"= ", end="")
+                break
+            print("x ", end="")
+        return fat
+            
+    else:
+        for c in range(num,0,-1):
+            if c < 5:
+                fat *= c
+        return fat
+
+        
+        
+    
+print("-" * 30)   
+print(fatorial(5, True))
+        
+        
+    
+        
+    
+        
+    
+    
+        
+        
+        
+    
+        
         
         
 
-fatorial(5)
-print()
-    
