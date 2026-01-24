c5af0d2ce96aeb90206740f31ea3e2aa11ed0820
Humberto <humbertoetiago@gmail.com>
2026-01-23 22:50:18 -0300

    Gerenciando arquivos

--- mundo 3/exer107/teste.py
+++
@@ -1,7 +0,0 @@
-import moeda
-
-p = float(input("Digite o valor: "))
-print(f"Metade de R${p} e R${moeda.metade(p)} ")
-print(f"O dobro de R${p} e R${moeda.dobro(p)}")
-print(f"Aumento de 10% de R${p} e R${moeda.aumentar(p)}")
-print(f"Reduzindo 13% de R${p} e R${moeda.diminuir(p)}")
