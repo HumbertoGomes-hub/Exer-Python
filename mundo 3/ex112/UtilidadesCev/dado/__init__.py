b88e9467e726c9894f9f5f5cedf5a43b44d81752
Humberto <humbertoetiago@gmail.com>
2026-01-24 02:24:11 -0300

    if = elif

--- mundo 3/ex112/UtilidadesCev/dado/__init__.py
+++ mundo 3/ex112/UtilidadesCev/dado/__init__.py
@@ -5,12 +5,13 @@ def leiaDinheiro(msg):
         if valor.isalpha():
             print(f"\033[1;31mERRO VALOR '{valor}' INVALIDO!\033[m")
     
-        if valor.strip() == "":
+        elif valor.strip() == "":
             print("\033[1;31mERRO VALOR INVALIDO!\033[m")
         else:
             res = float(valor)
             break
     return res
+
 
 
 
